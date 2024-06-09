9e3422ffa8df4137800a1a729fefa5ec6b04ffa6
Marcel Schoombee <marcelsch01@yahoo.com>
2024-06-09 10:58:23 +0200

    isOpen to boolean

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
+
+let isOpen: boolean
 
 const reviews = [
     {
