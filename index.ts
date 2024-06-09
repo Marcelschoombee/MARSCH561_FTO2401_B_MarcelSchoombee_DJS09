fb21321695de273fda1e52951280b364f1da228e
Marcel Schoombee <marcelsch01@yahoo.com>
2024-06-09 10:48:55 +0200

    Fixed code

--- index.ts
+++ index.ts
@@ -31,3 +31,20 @@ function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean)
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+// Fixed  code
+
+const you = {
+    userName: 'Bobby',
+    isReturning: true,
+}
+
+
+function populateUser(isReturning : boolean, userName : string ) {
+    if (isReturning){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName)
