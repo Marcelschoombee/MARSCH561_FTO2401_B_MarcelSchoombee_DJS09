465c1f96a053fe63606e6a7c91686d17cb92eaf3
Marcel Schoombee <marcelsch01@yahoo.com>
2024-06-09 14:12:41 +0200

    Enums Type

--- utils.ts
+++ utils.ts
@@ -1,11 +1,11 @@
 const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
-
+import { LoyaltyUser } from './enums'
 // Number Types and String type
 // Boolean type
-export function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean) {
-    const iconDisplay = isLoyalty ? '⭐' : ''
+export function showReviewTotal (value : number, reviewer: string, isLoyalty : LoyaltyUser) {
+    const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
     reviewTotalDisplay.innerHTML = 'Review total' + value.toString() + '| last reviewed by ' + 
     reviewer + iconDisplay
 }
