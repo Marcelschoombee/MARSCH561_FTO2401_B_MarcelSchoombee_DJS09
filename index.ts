a92f1e8c25dc02b0d29b2306a2b9c80aa7ad63f6
Marcel Schoombee <marcelsch01@yahoo.com>
2024-06-09 11:19:28 +0200

    Array type

--- index.ts
+++ index.ts
@@ -43,11 +43,13 @@ const you: {
     lastName: string;
     isReturning: boolean;
     age: number;
+    stayedAt: string[];
 } = {
    firstName: 'Bobby',
    lastName: 'Brown',
    isReturning: true,
-   age: 35
+   age: 35,
+   stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
 
