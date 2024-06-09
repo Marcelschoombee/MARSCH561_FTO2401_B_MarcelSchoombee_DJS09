afbcee213524e6d7413e36dde54fb5683447e173
Marcel Schoombee <marcelsch01@yahoo.com>
2024-06-09 15:40:08 +0200

    Classes

--- index.ts
+++ index.ts
@@ -48,19 +48,22 @@ const you: {
    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
-const properties : {
+interface Property {
     image: string;
     title: string;
-    price: number;
+    price: Price;
     location: {
         firstLine: string;
         city: string;
-        code: number;
+        code: number | string;
         country: Country;
-    };
-    contact: [number, string];
+    }
+    contact: [ number, string];
     isAvailable: boolean;
-}[] = [
+}
+
+const properties : Property[] = [ 
+    
      {image: 'images/colombia-property.jpg',
         title: 'Colombian Shack',
         price: 45,
@@ -76,7 +79,7 @@ const properties : {
     {
         image: 'images/poland-property.jpg',
         title: 'Polish Cottage',
-        price: 34,
+        price: 30,
         location: {
             firstLine: 'no 23',
             city: 'Gdansk',
@@ -89,7 +92,7 @@ const properties : {
         {
             image: 'images/london-property.jpg',
             title: 'London Flat',
-        price: 23,
+        price: 25,
         location: {
             firstLine: 'flat 15',
             city: 'London',
@@ -156,4 +159,27 @@ let currentLocation : [string, string, number] = ['London', '11.03', 17]
 footer.innerHTML = currentLocation[0] + ' ' + currentLocation[1] + ' ' + currentLocation[2] + '°'
 
 
+class MainProperty {
+    src: string
+    title: string
+    reviews: Review[]
+    constructor(src: string, title: string, reviews: Review[]) {
+        this.src = src
+        this.title = title
+        this.reviews = reviews
+    }
+}
 
+let yourMainProperty = new MainProperty('images/italian-property.jpg', 
+    'Italian House',
+    [{
+        name: 'Olive',
+        stars: 5,
+        loyaltyUser: LoyaltyUser.GOLD_USER,
+        date: '12-04-2021'
+    }])
+
+const mainImageContainer = document.querySelector('.main-image')
+const image = document.createElement('img')
+image.setAttribute('src', yourMainProperty.src)
+mainImageContainer.appendChild(image)
