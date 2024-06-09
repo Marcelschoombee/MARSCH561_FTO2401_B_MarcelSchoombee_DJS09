bf5a4e044509ec696d663a0b995aa10ff9466fb6
Marcel Schoombee <marcelsch01@yahoo.com>
2024-06-09 11:48:29 +0200

    Used images

--- index.ts
+++ index.ts
@@ -58,8 +58,7 @@ const properties : {
     contact: string;
     isAvailable: boolean;
 }[] = [
-     {
-        image: '',
+     {image: 'images/colombia-property.jpg',
         title: 'Colombian Shack',
         price: 45,
         location: {
@@ -72,7 +71,7 @@ const properties : {
         isAvailable: true  
     },
     {
-        image: '',
+        image: 'images/poland-property.jpg',
         title: 'Polish Cottage',
         price: 34,
         location: {
@@ -83,10 +82,10 @@ const properties : {
         },
         contact: 'garydavis@hotmail.com',
         isAvailable: false 
-    }
+    },
         {
-        image: '',
-        title: 'London Flat',
+            image: 'images/london-property.jpg',
+            title: 'London Flat',
         price: 23,
         location: {
             firstLine: 'flat 15',
