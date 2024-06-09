8cbc018219ee21f34b77c25be993a315056d37bb
Marcel Schoombee <marcelsch01@yahoo.com>
2024-06-09 16:48:29 +0200

    Last commit

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { showReviewTotal, populateUser, showDetails, getTopTwoReviews} from './u
 import { Permissions , LoyaltyUser } from './enums'
 import { Review, Property } from './interfaces'
 import MainProperty from './classes' 
+
 const propertyContainer = document.querySelector('.properties')
 const reviewContainer = document.querySelector('.reviews')
 const container = document.querySelector('.container')
